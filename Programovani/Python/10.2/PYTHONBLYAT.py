a17b643402dc1715a96df0a9e7dcb1f318d203ab
Kyuu <s3czsk@seznam.cz>
2026-02-10 08:35:45 +0100

    žzščerh¨d fúcv poihcv

---
+++ Programovani/Python/10.2/PYTHONBLYAT.py
@@ -0,0 +1,17 @@
+roblox=int(input("zadejte vysku:"))
+minecraft=int(input("Zadejte vek:"))
+fornite=0
+
+if roblox >=87:
+    print("trpasliku vyfič")
+else:
+    if minecraft<=12:
+        fortnite += 100
+
+    elif minecraft <=18:
+        fortnite +=100
+
+    elif minecraft <=65:
+        fortnite +=150
+
+print(fortnite)
