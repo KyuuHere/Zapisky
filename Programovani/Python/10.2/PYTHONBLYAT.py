1319e78a6cf90f6debe2239a3d0ef587c9a9f324
Kyuu <s3czsk@seznam.cz>
2026-02-10 09:38:52 +0100

    HRRRR

--- Programovani/Python/10.2/PYTHONBLYAT.py
+++ Programovani/Python/10.2/PYTHONBLYAT.py
@@ -1,12 +1,13 @@
 roblox=int(input("zadejte vysku:"))
 minecraft=int(input("Zadejte vek:"))
-fornite=0
+fortnite=0
 
-if roblox >=87:
+if roblox <=87:
     print("trpasliku vyfič")
+    
 else:
     if minecraft<=12:
-        fortnite += 100
+        fortnite += 50
 
     elif minecraft <=18:
         fortnite +=100
@@ -14,4 +15,7 @@ else:
     elif minecraft <=65:
         fortnite +=150
 
+string=input("Fotka?: (ano/ne)")
+if string=="ano":
+    fortnite+=40
 print(fortnite)
---
+++ Programovani/Python/10.2/cyklus.py
@@ -0,0 +1,10 @@
+import random
+roblox=10
+
+minecraft=random.randint(1,10)
+print("Gamba 1-10")
+while(True):
+    Challanger2OES=int(input("odhad:"))
+    if Challanger2OES==minecraft:
+        print("Vyhral")
+        break   
