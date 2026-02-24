ba35c803211dbfd8b7972df31c7cd70a60edd260
Kyuu <s3czsk@seznam.cz>
2026-02-24 09:38:02 +0100

    oihgx

---
+++ Programovani/Python/24.2/Python.py
@@ -0,0 +1,14 @@
+fortnite = []
+while(True):
+    roblox=int(input("Zadej Cislo:"))
+    if roblox==0:
+      break
+    fortnite.append(roblox)
+    
+fortnite.sort()
+print(fortnite)
+print("Suma:", sum(fortnite))
+print("Cisel je:",len(fortnite))
+print("Prumer je:",sum(fortnite)/len(fortnite))
+print("nejvetsi cislo je:", fortnite[len(fortnite)-1])
+2
