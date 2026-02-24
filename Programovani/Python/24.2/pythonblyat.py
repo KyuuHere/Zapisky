278b98561cc3c0b89709cc794f910f4b999ece1a
Kyuu <s3czsk@seznam.cz>
2026-02-24 09:37:45 +0100

    fortnite

--- Programovani/Python/24.2/pythonblyat.py
+++ Programovani/Python/24.2/pythonblyat.py
@@ -9,3 +9,25 @@ roblox=50
 while(roblox!=100):
         print(roblox)
         roblox+=5
+
+roblox=int(input("Zadej prvni cislo:"))
+minecraft=int(input("Zadejte druhe cislo"))
+fortnite=int(input("Zadejte krok"))
+
+p_cisel=0
+soucet=0
+
+if (roblox<minecraft):
+    for i in range(roblox,minecraft+1, fortnite):
+        print(i)
+        p_cisel+=1
+        soucet+=i
+
+else:
+    for i in range(roblox,minecraft-1,fortnite*(-1)):
+        print(i)
+        p_cisel+=1
+        soucet+=i
+
+prumer=soucet/p_cisel
+print(prumer)
