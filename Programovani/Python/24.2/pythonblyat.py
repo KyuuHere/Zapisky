ba3e7b52ae6d1de9418e6c23d7b9ff9614d248cf
Kyuu <s3czsk@seznam.cz>
2026-02-24 08:30:20 +0100

    it

---
+++ Programovani/Python/24.2/pythonblyat.py
@@ -0,0 +1,11 @@
+for roblox in range(50,100, 5):
+    print(roblox)
+
+for roblox in range(100,50 -5):
+    print(roblox)
+
+
+roblox=50
+while(roblox!=100):
+        print(roblox)
+        roblox+=5
